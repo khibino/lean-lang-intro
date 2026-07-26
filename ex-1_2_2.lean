6b79bfd2faa67a93fd22824d5d4e74db5ccfb4db
Kei Hibino <ex8k.hibino@gmail.com>
2026-07-26 14:43:23 +0900

    1.2.2

---
+++ ex-1_2_2.lean
@@ -0,0 +1,23 @@
+/-- リスト内包表記 -/
+declare_syntax_cat compClause
+
+syntax "for " term " in " term : compClause
+syntax "if " term : compClause
+syntax "[" term " | " compClause,* "]" : term
+
+macro_rules
+  | `([$t |]) => `([$t])
+  | `([$t | for $x in $xs]) => `(List.map (fun $x => $t) $xs)
+  | `([$t | if $x]) => `(if $x then [$t] else [])
+  | `([$t | $c, $cs,*]) => `(List.flatten [[$t | $cs,*] | $c])
+
+-- for 構文のテスト
+#guard [x ^ 2 | for x in [1, 2, 3, 4, 5]] = [1, 4, 9, 16, 25]
+
+-- 二重の for 構文のテスト
+#guard
+  let lhs := [(x, y) | for x in [1, 2, 3], for y in [4, 5]]
+  let rhs := [(1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3, 5)]
+  lhs = rhs
+
+#guard [x | for x in [1, 2, 3], if x < 2] = [1]
